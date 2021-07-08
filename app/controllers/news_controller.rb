3b2bb47a8538e3e9c9e328517f0b9761984cd8f9
Aiden Isaia <aidenisaia@gmail.com>
2021-07-07 19:33:00 -0700

    api key and encrypting

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,7 @@
 class NewsController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=tesla&from=2021-06-08&sortBy=publishedAt&apiKey=#{Rails.application.credentials.news_api}")
+
+    render json: response.parse(:json)
+  end
 end
